7f1a3f604ef35037db70678af5599be56706c9eb
Krishna <kisna_kn@rediffmail.com>
2019-11-21 19:34:54 +0530

    Excursion Update

--- app/src/main/java/com/org/checkweather/view/views/ExcursionFormActivity.java
+++ app/src/main/java/com/org/checkweather/view/views/ExcursionFormActivity.java
@@ -1,29 +1,46 @@
 package com.org.checkweather.view.views;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.org.checkweather.R;
+import com.org.checkweather.view.adapters.ExcursionAdapter;
 import com.org.checkweather.view.model.Excursion;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class ExcursionFormActivity extends AppCompatActivity {
+
+    private static final int ADD_WEATHER_REQUEST = 2 ;
 
     private EditText edtSourceName, edtDestName, edtTripPrice, edtTripStartDate, edtTripEndDate, edtTripWeather;
 
     private ImageView imgCalFrom, imgCalTo, imgWeather;
 
     private Button btnSave;
+
+    private Calendar calendar;
+    private SimpleDateFormat dateFormat;
+    private int mYear = 0, mMonth = 0, mDay = 0;
+    private String strDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,24 +60,35 @@ public class ExcursionFormActivity extends AppCompatActivity {
         imgCalTo = findViewById(R.id.imgCalEndDate);
         imgWeather = findViewById(R.id.imgWeather);
 
+
+        calendar = Calendar.getInstance();
+        mYear = calendar.get(Calendar.YEAR);
+        mMonth = calendar.get(Calendar.MONTH);
+        mDay = calendar.get(Calendar.DAY_OF_MONTH);
+        dateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);//Locale.getDefault());
+        strDate = dateFormat.format(calendar.getTime());
+
+
+
         imgCalFrom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                setDate(edtTripStartDate);
             }
         });
 
         imgCalTo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                setDate(edtTripEndDate);
             }
         });
 
         imgWeather.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(ExcursionFormActivity.this, MainActivity.class);
+                startActivityForResult(intent, ADD_WEATHER_REQUEST);
             }
         });
 
@@ -132,5 +160,40 @@ public class ExcursionFormActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void setDate(final EditText edtView){
+
+            mYear = calendar.get(Calendar.YEAR);
+            mMonth = calendar.get(Calendar.MONTH);
+            mDay = calendar.get(Calendar.DAY_OF_MONTH);
+            // Launch Date Picker Dialog
+            final DatePickerDialog lDpd = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(@NonNull DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                            // Display Selected date
+                            if (view.isShown()) {
+                                calendar.set(Calendar.YEAR, year);
+                                calendar.set(Calendar.MONTH, monthOfYear);
+                                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                                strDate = dateFormat.format(calendar.getTime());
+                                mYear = year;
+                                mMonth = monthOfYear;
+                                mDay = dayOfMonth;
+                                edtView.setText(dateFormat.format(calendar.getTime()));
+                            }
+                        }
+                    }, mYear, mMonth, mDay);
+            lDpd.show();
+        }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+
+
+        }
     }
 }
